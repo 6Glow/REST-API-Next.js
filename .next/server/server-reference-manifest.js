ac0783a9a88484d0f8f55f59030f9f656c725bae
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-05 23:54:32 +0300

    feat: implement PATCH endpoint for updating user information

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"V0n1iDzGq/b/IDzz0YdldjDs22sYjihiWQuXR2dotNY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NI8VjJQToMkkyn6aVW34aRF8DwBzbEiztXA21h7u650=\"\n}"
--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,6 +1,10 @@
 import connect from "@/lib/db";
 import User from "@/lib/models/user";
 import { NextResponse } from "next/server"
+import { Types } from "mongoose";
+
+
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try{
@@ -32,3 +36,49 @@ export const POST = async (request: Request) => {
     })
   }
 }
+
+
+export const PATCH = async(request: Request) => {
+  try{
+    const body = await request.json();
+    const {userId, newUsername} = body;
+
+    await connect();
+    if(!userId || !newUsername) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or new username not found" }), 
+        { status: 400 }
+      );
+    }
+
+    if(!Types.ObjectId.isValid(userId)){
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid User id" }),
+        { status: 400 }
+      );
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      {_id: new ObjectId(userId)},
+      { username: newUsername },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database"}),
+        { status: 400 }
+      )  
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is updated", user: updatedUser }),
+      { status: 200 }
+    )
+
+  } catch (error: any) {
+    return new NextResponse("Error is updating user" + error.message, {
+      status :  500,
+    });
+  }
+};
