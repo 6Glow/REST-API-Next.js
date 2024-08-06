5cd8b364f7f6c332f23112428ef9fe748d6192cc
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-06 12:49:21 +0300

    feat: implement GET categories endpoint

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v5nKiokrUeiaRAWN9AB+juoTY0ighM/T2fUKMDl2kZo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GsOUPejA1x6BMl2jTJTRPGY1rqaqa9wROE/+6OnLmRo=\"\n}"
