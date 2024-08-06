cb3fc40bfde8660353dff7b567606617b0375121
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-06 22:22:52 +0300

    feat: implement search blogs endpoint with keywords

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -11,6 +11,7 @@ export const GET = async(request: Request) => {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get('userId');
     const categoryId = searchParams.get('categoryId');
+    const searchKeywords = searchParams.get("keywords") as string;
 
     if(!userId || !Types.ObjectId.isValid(userId)){
       return new NextResponse(
@@ -51,6 +52,17 @@ export const GET = async(request: Request) => {
     user: new Types.ObjectId(userId),
     category: new Types.ObjectId(categoryId),
   };
+
+  if(searchKeywords) {
+    filter.$or = [
+      {
+        title: { $regex: searchKeywords, $options:  "i"},
+      },
+      {
+        description: { $regex: searchKeywords, $options:  "i"},
+      }
+    ]
+  }
 
   const blogs = await Blog.find(filter);
   return new NextResponse(JSON.stringify({ blogs }), {
